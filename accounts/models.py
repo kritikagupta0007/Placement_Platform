7c46106de3b0fcf2c6f96cfaaff1bd1745efade1
kritikagupta0007 <gupta.kritika0007@gmail.com>
2022-01-30 22:52:22 +0530

    Update model

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,24 @@
+import datetime
 from django.db import models
 
 # Create your models here.
 class Register(models.Model):
-    first_name = models.CharField(max_length=50)
-    father_name = models.CharField(max_length=50)
-    mother_name = models.CharField(max_length=50)
-    aadhar_number = models.IntegerField()
+    university_rollnumber = models.IntegerField(default = 0)
+    admission_number = models.CharField(max_length=9, default='', editable=True)
+    full_name = models.CharField(max_length=30,default='', editable=True)
+    father_name = models.CharField(max_length=30, default='', editable=True)
+    mother_name = models.CharField(max_length=30, default='', editable=True)
+    aadhar_number = models.IntegerField(default = 0)
+    date_Of_Birth = models.DateField(("Date"), default=datetime.date.today)
+    gender = models.CharField(max_length=6, default='', editable=True)
+    class_10th_percentage = models.IntegerField(default = 0)
+    class_12th_percentage = models.IntegerField(default = 0)
+    btech_percentage = models.IntegerField(default = 0)
+    btech_branch = models.CharField(max_length=6, default='', editable=True)
+    phone_number = models.IntegerField(default = 0)
+    parents_phone_number = models.IntegerField(default=0)
+    upload_Resume = models.FileField(upload_to=None, default= '',max_length=254)
+    email = models.EmailField(max_length=30, default='', editable=True)
+    password = models.CharField(max_length=30, default='', editable=True)
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.forms import PasswordInput
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import auth
+
+from .models import Register
 
 # Create your views here.
 
@@ -12,22 +14,43 @@ def course(request):
 def register(request):
 
     if request.method == "POST":
-        name = request.POST['name']
+        university_roll_number = request.POST['university_roll_number']
+        admission_number = request.POST['admission_number']
+        name = request.POST['full_name']
+        father_name = request.POST['father_name']
+        mother_name = request.POST['mother_name']
+        aadhar_number = request.POST['aadhar_number']
+        dateOfBirth = request.POST['dateOfBirth']
+        marks1 = request.POST['marks1']
+        marks2 = request.POST['marks2']
+        marks3 = request.POST['marks3']
+        branch = request.POST['branch']
+        phone_number = request.POST['phone_number']
+        parents_number = request.POST['parents_number']
+        upload_resume = request.POST['upload_resume']
         email = request.POST['email']
-        username = request.POST['username']
-        psw = request.POST['psw']
-        psw2 = request.POST['psw2']
+        psw = request.POST['password1']
+        psw2 = request.POST['password2']
 
         if psw == psw2:
-            if User.objects.filter(username = username).exists():
-               messages.info(request, 'Username exist')
+            if Register.objects.filter(university_rollnumber = university_roll_number).exists():
+               messages.info(request, 'University Roll Number exist')
                return redirect('register')
-            elif User.objects.filter(email = email).exists():
+            elif Register.objects.filter(admission_number = admission_number).exists():
+                messages.info(request, 'Admission Number exist')
+                return redirect('register')
+            elif Register.objects.filter(aadhar_number = aadhar_number).exists():
+                messages.info(request, 'Aadhar Number exist')
+                return redirect('register')
+            elif Register.objects.filter(phone_number = phone_number).exists():
+                messages.info(request, 'Phone number exist')
+                return redirect('register')
+            elif Register.objects.filter(email = email).exists():
                 messages.info(request, 'email exist')
                 return redirect('register')
             else:
-                user = User.objects.create_user(first_name=name, email=email, username = username, password=psw)
-                user.save()
+                register = Register.objects.auto_created(university_rollnumber =  university_roll_number, admission_number=admission_number,full_name=name, email=email, father_name = father_name, password=psw,mother_name = mother_name, aadhar_number =  aadhar_number, date_Of_Birth = dateOfBirth, class_10th_percentage = marks1,class_12th_percentage = marks2,btech_percentage = marks3,  btech_branch = branch ,phone_number = phone_number,parents_phone_number = parents_number, upload_Resume =  upload_resume)
+                register.save()
                 print("user created")
                 return redirect('login')
         else:
@@ -45,10 +68,10 @@ def login(request):
         email = request.POST['email']
         password = request.POST['password']
 
-        user = auth.authenticate(email=email, password=password)
+        register = auth.authenticate(email=email, password=password)
 
-        if user is not None:
-            auth.login(request, user)
+        if register is not None:
+            auth.login(request, register)
             return redirect('/') 
 
         else:
