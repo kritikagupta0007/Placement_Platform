2b80b62536c0dc4178e6ca96b3956b6f7e4fcbf5
kritikagupta0007 <gupta.kritika0007@gmail.com>
2022-02-10 11:44:21 +0530

    addjob

--- job/views.py
+++ job/views.py
@@ -9,13 +9,13 @@ def tporole(request):
 
 def addjob(request):
     if request.method == 'POST':
-        cname = request.POST.get["companyname"]
-        profile = request.POST.get["profile"]
-        package = request.POST.get["package"]
-        eligible = request.POST.get["eligible"]
-        drive_date = request.POST.get["date"]
-        last_date = request.POST.get["lastDate"]
-        status = request.POST.get["status"]
+        cname = request.POST.get("companyname")
+        profile = request.POST.get("profile")
+        package = request.POST.get("package")
+        eligible = request.POST.get("eligible")
+        drive_date = request.POST.get("date")
+        last_date = request.POST.get("lastDate")
+        status = request.POST.get("status")
         add = JobForm(company=cname, profile=profile, package=package, eligibility=eligible, drive_date=drive_date, last_date=last_date, status=status)
         add.save()
         return redirect("tporole")
